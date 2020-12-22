5489cb44f4ec1e5a5633dfdba1381c34ea961776
Shady <shady.ber9@gmail.com>
2020-12-22 12:01:18 +0300

    first commit database setup migragation and frameworks

---
+++ database/migrations/2020_12_22_084605_create_abouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('about');
+            $table->longText('mission')->nullable();
+            $table->longText('vision')->nullable();
+            $table->longText('value')->nullable();
+            $table->longText('goal')->nullable();
+            $table->longText('background')->nullable();
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource('/address', App\Http\Controllers\AddressController::class);
+Route::resource('/station', App\Http\Controllers\StationController::class);
+Route::resource('/album', App\Http\Controllers\AlbumController::class);
+Route::resource('/photo', App\Http\Controllers\PhotoController::class);
+Route::resource('/about', App\Http\Controllers\AboutController::class);
+Route::resource('/slide', App\Http\Controllers\SlideController::class);
+Route::resource('/product', App\Http\Controllers\ProductController::class);
+Route::resource('/order', App\Http\Controllers\OrderController::class);
+Route::resource('/message', App\Http\Controllers\MessageController::class);
+Route::resource('/service', App\Http\Controllers\ServiceController::class);
+Route::resource('/blog', App\Http\Controllers\BlogController::class);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
